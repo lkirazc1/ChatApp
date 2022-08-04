e6bb35a6bac689df8ede6c737e43be415b29908d
lkirazc1 <97258501+lkirazc1@users.noreply.github.com>
2022-08-04 03:25:18 -0700

    Create collapsemenu.js

---
+++ static/collapsemenu.js
@@ -0,0 +1,10 @@
+function collapseMenu()
+{
+    //TODO: collapse menu to show only the icons of the contacts instead of also the name and vice versa
+    console.log("collapseMenu");
+}
+
+
+
+
+document.getElementById("collapsemenu").addEventListener("click", collapseMenu)
