a8457646eeda3926fbcfe3d2f62509fcf1fdf423
Eeshan <97463443+ekhande1@users.noreply.github.com>
2022-09-05 09:22:37 -0400

    Instead of using the CS50 library

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import re
+import sqlite3
 
-from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
@@ -24,7 +24,8 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///chat.db")
+connection = sqlite3.connect("chat.db")
+db = connection.cursor()
 
 @app.after_request
 def after_request(response):
